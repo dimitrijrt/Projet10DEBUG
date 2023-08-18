c14a1ab2014b82309913f77ea0b051459baaff87
dimitrijrt <dimitrijacquart@gmail.com>
2023-08-18 15:11:33 +0200

    dot index

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -41,12 +41,12 @@ const Slider = () => {
             
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((radioIdx) => (
+              {byDateDesc.map((_,radioIdx) => (
                 <input
                   key={`${event.id}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
